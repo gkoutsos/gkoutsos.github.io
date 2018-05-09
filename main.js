25bcb52e75f330db69165689441a363772e33510
gkoutsos <36250848+gkoutsos@users.noreply.github.com>
2018-05-09 11:01:32 -0500

    BLE Data Transfer

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('WebComponentsReady', function() {
+
+    var batteryDevice = document.querySelector('platinum-bluetooth-device');
+    var button = document.querySelector('paper-button');
+  
+    button.addEventListener('click', function() {
+      console.log('Requesting a bluetooth device advertising battery service...');
+  
+      batteryDevice.request().then(function(device) {
+        console.log('A bluetooth device has been found!');
+        console.log('Device Name: ' + device.name);
+  
+        // Connect to the device and read battery level.
+        var batteryLevel = batteryDevice.querySelector('platinum-bluetooth-characteristic');
+        return batteryLevel.read().then(function() {
+          document.querySelector('paper-toast').show();
+        });
+  
+      })
+      .catch(function(error) {
+        console.error('Argh! ', error);
+      });
+    });
+  });
+  
+  var template = document.querySelector('template');
+  template.computeCharacteristic = function(batteryLevel) {
+    return ('Battery Level is ' + batteryLevel.getUint8(0) + '%');
+  };
