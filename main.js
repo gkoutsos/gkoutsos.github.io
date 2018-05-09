68bde3e4b06682afbced42b98d71f5bef27784bb
gkoutsos <36250848+gkoutsos@users.noreply.github.com>
2018-05-09 11:25:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,114 @@
-document.addEventListener('WebComponentsReady', function() {
+function onButtonClick() {
+  log('Requesting any Bluetooth Device...');
+  navigator.bluetooth.requestDevice({
+   // filters: [...] <- Prefer filters to save energy & show relevant devices.
+      acceptAllDevices: true,
+      optionalServices: ['device_information']})
+  .then(device => {
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    log('Getting Device Information Service...');
+    return server.getPrimaryService('device_information');
+  })
+  .then(service => {
+    log('Getting Device Information Characteristics...');
+    return service.getCharacteristics();
+  })
+  .then(characteristics => {
+    let queue = Promise.resolve();
+    let decoder = new TextDecoder('utf-8');
+    characteristics.forEach(characteristic => {
+      switch (characteristic.uuid) {
 
-    var batteryDevice = document.querySelector('platinum-bluetooth-device');
-    var button = document.querySelector('paper-button');
-  
-    button.addEventListener('click', function() {
-      console.log('Requesting a bluetooth device advertising battery service...');
-  
-      batteryDevice.request().then(function(device) {
-        console.log('A bluetooth device has been found!');
-        console.log('Device Name: ' + device.name);
-  
-        // Connect to the device and read battery level.
-        var batteryLevel = batteryDevice.querySelector('platinum-bluetooth-characteristic');
-        return batteryLevel.read().then(function() {
-          document.querySelector('paper-toast').show();
-        });
-  
-      })
-      .catch(function(error) {
-        console.error('Argh! ', error);
-      });
+        case BluetoothUUID.getCharacteristic('manufacturer_name_string'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> Manufacturer Name String: ' + decoder.decode(value));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('model_number_string'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> Model Number String: ' + decoder.decode(value));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('hardware_revision_string'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> Hardware Revision String: ' + decoder.decode(value));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('firmware_revision_string'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> Firmware Revision String: ' + decoder.decode(value));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('software_revision_string'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> Software Revision String: ' + decoder.decode(value));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('system_id'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> System ID: ');
+            log('  > Manufacturer Identifier: ' +
+                padHex(value.getUint8(4)) + padHex(value.getUint8(3)) +
+                padHex(value.getUint8(2)) + padHex(value.getUint8(1)) +
+                padHex(value.getUint8(0)));
+            log('  > Organizationally Unique Identifier: ' +
+                padHex(value.getUint8(7)) + padHex(value.getUint8(6)) +
+                padHex(value.getUint8(5)));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('ieee_11073-20601_regulatory_certification_data_list'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> IEEE 11073-20601 Regulatory Certification Data List: ' +
+                decoder.decode(value));
+          });
+          break;
+
+        case BluetoothUUID.getCharacteristic('pnp_id'):
+          queue = queue.then(_ => characteristic.readValue()).then(value => {
+            log('> PnP ID:');
+            log('  > Vendor ID Source: ' +
+                (value.getUint8(0) === 1 ? 'Bluetooth' : 'USB'));
+            if (value.getUint8(0) === 1) {
+              log('  > Vendor ID: ' +
+                  (value.getUint8(1) | value.getUint8(2) << 8));
+            } else {
+              log('  > Vendor ID: ' +
+                  getUsbVendorName(value.getUint8(1) | value.getUint8(2) << 8));
+            }
+            log('  > Product ID: ' +
+                (value.getUint8(3) | value.getUint8(4) << 8));
+            log('  > Product Version: ' +
+                (value.getUint8(5) | value.getUint8(6) << 8));
+          });
+          break;
+
+        default: log('> Unknown Characteristic: ' + characteristic.uuid);
+      }
     });
+    return queue;
+  })
+  .catch(error => {
+    log('Argh! ' + error);
   });
-  
-  var template = document.querySelector('template');
-  template.computeCharacteristic = function(batteryLevel) {
-    return ('Battery Level is ' + batteryLevel.getUint8(0) + '%');
-  };
+}
+
+/* Utils */
+
+function padHex(value) {
+  return ('00' + value.toString(16).toUpperCase()).slice(-2);
+}
+
+function getUsbVendorName(value) {
+  // Check out page source to see what valueToUsbVendorName object is.
+  return value +
+      (value in valueToUsbVendorName ? ' (' + valueToUsbVendorName[value] + ')' : '');
+}
